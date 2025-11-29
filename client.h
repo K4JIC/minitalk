4fe5f8d23c4b43ee016b0f94d2bafbfc0137b4e0
Tomoya Ozaki <tozaki@c4r3s11.42tokyo.jp>
2025-11-29 15:41:59 +0900

    2025-11-29

--- client.h
+++ client.h
@@ -6,7 +6,7 @@
 /*   By: tozaki <tozaki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 16:33:43 by tozaki            #+#    #+#             */
-/*   Updated: 2025/11/29 13:49:24 by tozaki           ###   ########.fr       */
+/*   Updated: 2025/11/29 13:56:47 by tozaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@
 typedef struct s_client_status
 {
 	int	ack_received;
-	int	pid_char_cnt;
-	int	pid_bit_cnt;
 	int	char_cnt;
 	int	bit_cnt;
 }		t_client_status;
--- client2.c
+++ client2.c
@@ -1,79 +1,75 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   client2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tozaki <tozaki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 20:54:07 by tozaki            #+#    #+#             */
-/*   Updated: 2025/11/29 14:03:24 by tozaki           ###   ########.fr       */
+/*   Updated: 2025/11/29 15:40:45 by tozaki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "client.h"
 #include "libft/libft.h"
 #include <stdlib.h>
 
+typedef struct s_client_status
+{
+	int		ack_received;
+	int		char_cnt;
+	int		bit_cnt;
+	int		s_pid;
+	char	*str;
+}		t_client_status;
+
 volatile t_client_status	g_client;
 
-void	ack_handler(int signo)
+void	send_bit(int signo)
 {
-	(void)signo;
-	g_client.ack_received = 1;
-}
+	int	bit;
 
-void	send_bit(int s_pid, int bit)
-{
+	if (signo != SIGUSR1)
+		return ;
+	if (g_client.bit_cnt == 8)
+	{
+		g_client.bit_cnt = 0;
+		if (!g_client.str[g_client.char_cnt])
+		{
+			g_client.char_cnt++;
+			return ;
+		}
+		g_client.char_cnt++;
+	}
+	bit = (g_client.str[g_client.char_cnt] >> g_client.bit_cnt) & 1;
+	g_client.bit_cnt++;
 	if (bit)
-		kill(s_pid, SIGUSR1);
+		kill(g_client.s_pid, SIGUSR1);
 	else
-		kill(s_pid, SIGUSR2);
-}
-
-void	send_char(int s_pid, char c)
-{
-	int	i;
-
-	i = 0;
-	while (i < 8)
-	{
-		g_client.ack_received = 0;
-		send_bit(s_pid, (c >> i) & 1);
-		while (!g_client.ack_received)
-			pause();
-		i++;
-	}
-}
-
-void	send_string(int s_pid, char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		send_char(s_pid, str[i]);
-		i++;
-	}
-	send_char(s_pid, '\0');
+		kill(g_client.s_pid, SIGUSR2);
 }
 
 int	main(int argc, char **argv)
 {
 	struct sigaction	act;
+	size_t				slen;
 
 	if (argc != 3)
 		return (1);
 	ft_bzero(&act, sizeof(struct sigaction));
-	act.sa_handler = ack_handler;
+	act.sa_handler = send_bit;
 	sigemptyset(&act.sa_mask);
 	sigaction(SIGUSR1, &act, NULL);
 	g_client.ack_received = 0;
 	g_client.char_cnt = 0;
 	g_client.bit_cnt = 0;
-	send_string(ft_atoi(argv[1]), argv[2]);
+	g_client.str = argv[2];
+	g_client.s_pid = ft_atoi(argv[1]);
+	slen = ft_strlen(argv[2]);
+	send_bit(SIGUSR1);
+	while (g_client.char_cnt <= (int)slen)
+		pause();
 	return (0);
 }
